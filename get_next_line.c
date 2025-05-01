7470a73825c192c23ac0320c9d2c1e9dcee19491
Octave Graf <cobra.morilles-0n@icloud.com>
2025-05-01 20:15:55 +0200

    started function

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,25 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 12:07:18 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/05/01 12:20:10 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/05/01 19:35:22 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 char	*get_next_line(int fd)
 {
-	// Lire le fichier
-	// 
+	static char		*buffer;
+	char			*result;
+	static size_t	i;
+	static size_t	size;
+
+	
+	size = 1024
 }
+
+/*
+ouvrir le fd
+detecter le \n
+allouer la memoire pour stocker les phrases
+*/
