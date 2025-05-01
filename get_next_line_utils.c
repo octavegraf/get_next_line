400fee4081c2f12eccf54a9d1d430fe7435680e1
Octave Graf <cobra.morilles-0n@icloud.com>
2025-05-01 20:13:13 +0200

    added function is-in-charset, cut string and started main

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,41 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 12:18:40 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/05/01 12:18:41 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/05/01 20:12:49 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+int	c_is_in_charset(char c, char set)
+{
+	if (c == set)
+		return (1);
+	return (0);
+}
+
+char	*cut_string(char *s, char set)
+{
+	size_t	i;
+	char	*result;
+
+	i = 0;
+	while (s[i] && !c_is_in_charset(s[i], set))
+		i++;
+	result = malloc(sizeof(char) * i);
+	i = 0;
+	while (s[i] && !c_is_in_charset(s[i], set))
+	{
+		result[i] = s[i];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+int	main(void)
+{
+	int	fd;
+
+	fd = open("lorem_ipsum", O_RDONLY);
+}
