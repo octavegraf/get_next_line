839a165293ae3887dd996528390299766b32818b
Octave Graf <cobra.morilles-0n@icloud.com>
2025-05-02 19:53:01 +0200

    removed strchr added strlen, modified cut_string function name, added strjoin

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 12:18:40 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/05/01 20:16:41 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/05/02 19:48:27 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,18 @@ int	c_is_in_charset(char c, char set)
 		return (1);
 	return (0);
 }
-char	*ft_strchr(const char *s, int c)
-{
-	char	*str;
-	int		i;
 
-	str = (char *)s;
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
 	i = 0;
-	while (str[i])
-	{
-		if (str[i] == (char)c)
-			return (str + i);
+	while (s[i])
 		i++;
-	}
-	if (!c)
-		return (str + i);
-	return (NULL);
+	return (i);
 }
 
-char	*cut_string(char *s, char set)
+char	*c_cut_string(char *s, char set)
 {
 	size_t	i;
 	char	*result;
@@ -55,9 +48,34 @@ char	*cut_string(char *s, char set)
 	return (result);
 }
 
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*s1s2;
+	size_t	size;
+	size_t	i;
+
+	size = ft_strlen(s1) + ft_strlen(s2);
+	s1s2 = malloc((size + 1) * sizeof(char));
+	if (!s1s2)
+		return (NULL);
+	i = 0;
+	while (i < ft_strlen(s1))
+	{
+		s1s2[i] = s1[i];
+		i++;
+	}
+	while ((i - ft_strlen(s1)) < ft_strlen(s2))
+	{
+		s1s2[i] = s2[i - ft_strlen(s1)];
+		i++;
+	}
+	s1s2[i] = '\0';
+	return (s1s2);
+}
+
 int	main(void)
 {
 	int	fd;
-
 	fd = open("lorem_ipsum", O_RDONLY);
+	
 }
