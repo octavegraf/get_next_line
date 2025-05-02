3c96c6f8ae9e7c408c045a643cc86d57b1ee16e5
Octave Graf <cobra.morilles-0n@icloud.com>
2025-05-02 19:53:28 +0200

    added buffer size and functions

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,24 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 12:21:12 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/05/01 20:17:02 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/05/02 19:48:58 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
 # include <unistd.h>
+# include <stdlib.h>
 # include <fcntl.h>
 
 char	*get_next_line(int fd);
-char	*cut_string(char *s, char set);
-
+char	*c_cut_string(char *s, char set);
+int		c_is_in_charset(char c, char set);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
 
 #endif
