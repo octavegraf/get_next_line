a02ebb58654abc0e064d16d064f1fcf386f3d414
Octave Graf <cobra.morilles-0n@icloud.com>
2025-05-03 10:37:44 +0200

    added str_is_in_charset

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 12:21:12 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/05/02 20:04:05 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/05/03 10:11:03 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@
 char	*get_next_line(int fd);
 char	*c_cut_string(char *s, char set);
 int		c_is_in_charset(char c, char set);
+int		str_is_in_charset(char *s, char set);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
