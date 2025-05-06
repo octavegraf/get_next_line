807c6f41ab0106f1d0560add297f2765eb9a184c
Octave Graf <ocgraf@student.42.fr>
2025-05-06 12:42:33 +0200

    protection on some mallocs

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 12:07:18 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/05/05 17:49:55 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/05/06 12:41:35 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,25 @@ char	*get_next_line(int fd)
 	if (!buffer)
 		return (NULL);
 	if (!temp)
-		temp = buffer;
+		temp = "";
 	fd_status = read(fd, buffer, BUFFER_SIZE);
 	while (!s_is_in_charset(temp, '\n') && fd_status > 0)
 	{
 		buffer[BUFFER_SIZE] = '\0';
 		temp = ft_strjoin(temp, buffer);
+		if (!temp)
+			return (NULL);
 		fd_status = read(fd, buffer, BUFFER_SIZE);
 	}
-	if (fd_status <= 0 && !*temp)
-	{
-		free(buffer);
-		free(temp);
+	if (fd_status <= 0 && !temp)
 		return (NULL);
-	}
+	free(buffer);
 	buffer = ft_substr(temp, 0, s_is_in_charset(temp, '\n'));
-	return (temp = ft_substr(temp, s_is_in_charset(temp, '\n') + 1,
-			ft_strlen(temp) - s_is_in_charset(temp, '\n')), buffer);
+	if (!s_is_in_charset(temp, '\n'))
+		return (buffer = ft_strjoin(temp, ""), free(temp), buffer);
+	temp = ft_substr(temp, s_is_in_charset(temp, '\n') + 1,
+			ft_strlen(temp) - s_is_in_charset(temp, '\n'));
+	if (!temp)
+		return (NULL);
+	return (buffer);
 }
