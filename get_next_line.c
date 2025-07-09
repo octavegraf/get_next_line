133e64079c5f115e841cc2ed58a054c70df72c78
Octave Graf <ocgraf@student.42.fr>
2025-07-09 16:28:36 +0200

    added a emergency cleanup

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ocgraf <ocgraf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 12:07:18 by ocgraf            #+#    #+#             */
-/*   Updated: 2025/06/09 14:57:09 by ocgraf           ###   ########.fr       */
+/*   Updated: 2025/07/09 16:28:05 by ocgraf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,30 +31,37 @@ char	*gnl_read_until_nl(int fd, char *current)
 			return (free(buffer), buffer = NULL, current);
 		buffer[read_status] = '\0';
 		temp = gnl_ft_strjoin(current, buffer);
+		if (!temp)
+			return (free(buffer), free(current), NULL);
 		free(current);
 		current = temp;
 	}
 	return (free(buffer), buffer = NULL, current);
 }
 
-static char	*gnl_cut_line(char **current)
+static char	*gnl_cut_line(char **current, int n)
 {
 	char	*result;
 	char	*temp;
-	int		n;
 
 	n = gnl_s_is_in_charset(*current, '\n');
 	if (n >= 0)
 	{
 		result = gnl_ft_substr(*current, 0, n + 1);
+		if (!result)
+			return (free(*current), *current = NULL, NULL);
 		temp = gnl_ft_substr(*current, n + 1,
 				((gnl_ft_strlen(*current) - n) - 1));
+		if (!temp)
+			return (free(*current), *current = NULL, free(result), NULL);
 		free(*current);
 		*current = temp;
 	}
 	else
 	{
 		result = gnl_ft_strdup(*current);
+		if (!result)
+			return (free(*current), *current = NULL, NULL);
 		free(*current);
 		*current = NULL;
 	}
@@ -66,9 +73,21 @@ char	*get_next_line(int fd)
 	static char	*current = NULL;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+		if (current)
+		{
+			free(current);
+			current = NULL;
+		}
 		return (NULL);
+	}
 	current = gnl_read_until_nl(fd, current);
 	if (!current || !*current)
 		return (free(current), current = NULL, NULL);
-	return (gnl_cut_line(&current));
+	return (gnl_cut_line(&current, 0));
+}
+
+void	gnl_cleanup(void)
+{
+	get_next_line(-1);
 }
